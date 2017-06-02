af90b2a98382763f5c95a2feed67bc4587f74398
Charishma Daddali <Charishma Daddali>
2017-06-01 23:05:30 -0400

    initial code with static json data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import { TenantsComponent } from './tenants/tenants.component';
 
 const routes: Routes = [
   {
     path: '',
     children: []
+  },
+  {
+    path: 'Dashboard',
+    component: DashboardComponent
+  },
+  {
+  path: 'Tenants',
+  component: TenantsComponent
   }
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+const Heroes = [
+{name:'Dashboard',
+  child:[{name:'home'}, {name:'about'}]},
+{name:'Tenants'},
+{name:'POs'},
+{name:'Admin'}
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
+
 export class AppComponent {
-  title = 'app works!';
+title = 'trackme';
+  heroes = Heroes;
 }
